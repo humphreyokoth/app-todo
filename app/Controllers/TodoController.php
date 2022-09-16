ed7aa0811525835025492ff3be5a7546626cbe3a
humphreyokoth <humpasp@gmail.com>
2022-09-16 13:47:54 +0300

    Feat:codeignite app

--- app/Controllers/TodoController.php
+++ app/Controllers/TodoController.php
@@ -27,13 +27,13 @@ class TodoController extends Controller
         $todoModel->insert($data);
         return $this->response->redirect(site_url('/todo-list'));
     }
-
+// Get item by id 
     public function getTodo($id = null){
         $todoModel = new todoModel();
         $data['todo_obj'] = $todoModel->where('id', $id)->first();
         return view('edit_todo', $data);
     }
-
+// Updating todo item
     public function update(){
         $todoModel = new todoModel();
         $id = $this->request->getVar('id');
@@ -44,7 +44,7 @@ class TodoController extends Controller
         $todoModel->update($id, $data);
         return $this->response->redirect(site_url('/todo-list'));
     }
- 
+//  Delete todo item
     public function delete($id = null){
         $todoModel = new todoModel();
         $data['todo'] = $todoModel->where('id', $id)->delete($id);
