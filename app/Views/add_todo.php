da693d81c0cdb6824880e9f6de4f06dd206b8ef7
humphreyokoth <humpasp@gmail.com>
2022-09-17 22:30:22 +0300

    Feat:codeignite app

--- app/Views/add_todo.php
+++ app/Views/add_todo.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-  <title>Codeigniter 4 CRUD Operations Example</title>
+  <title></title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <style>
     .error {
